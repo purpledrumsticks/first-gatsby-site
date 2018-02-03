56c1aa7e13f2945c82421f87266cdf6d2035550a
Inigo Montoya <inigo@MacBook-Pro.local>
2018-02-03 10:56:22 -0500

    master initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Default Starter`
+  },
+  pathPrefix: `/first-gatsby-site`,
+  plugins: [`gatsby-plugin-react-helmet`]
+};
---
+++ src/pages/page-3.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const ThirdPage = () => (
+  <div>
+    <h1>Hi from the thirst page</h1>
+    <p>Welcome to page 33333333333333</p>
+  </div>
+)
+
+export default ThirdPage
